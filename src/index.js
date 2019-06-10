8e1e4395d22cca6ea4dbfdd36fe3168f4cb590c5
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-06-10 14:56:00 +0100

    chore(setup): allow cross-origin requests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import config from './configs';
 import logger from './helpers/logger';
@@ -6,6 +7,11 @@ import appRouter from './router';
 
 const app = express();
 
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
