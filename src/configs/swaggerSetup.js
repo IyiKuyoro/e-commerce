2994db46013de58194932b8548350dad62473bc9
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-06-21 12:34:21 +0100

    chore(docs): create swagger docs

---
+++ src/configs/swaggerSetup.js
@@ -0,0 +1,16 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const options = {
+  swaggerDefinition: {
+    info: {
+      title: 'Shopmate API Docs',
+      version: '1.0.0',
+      description: 'This documentation contains all the endpoints currently available on Shopmate',
+    },
+    host: 'https://https://iyikuyoro-commerce.herokuapp.com',
+    basePath: '/api/v1',
+  },
+  apis: ['./docs/**/*.yml'],
+};
+
+export default swaggerJSDoc(options);
